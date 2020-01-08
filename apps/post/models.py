7045875b3cf8b2af4f8015e4acec7275712bc329
alireza <alirezashateri7@gmail.com>
2020-01-08 16:46:43 +0330

    Add Like model to Post app

--- apps/post/models.py
+++ apps/post/models.py
@@ -5,29 +5,36 @@ from django.db import models
 
 # Create your models here.
 
-class Post(models.Model):
 
-    def upload_path(self, filename):
-        return os.path.join('private/', self.profile.user.username, 'posts', str(self.id), filename)
-
-    profile = models.ForeignKey('account.Profile', related_name='posts', on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
+class UserReactionTemplate(models.Model):
+    from ..account.models import Profile
+    profile = models.ForeignKey(Profile, related_name='posts', on_delete=models.CASCADE)
     body = models.TextField()
-    media = models.FileField(upload_to=upload_path)
     create_date = models.DateTimeField(auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True)
 
 
-class Comment(models.Model):
+class Post(UserReactionTemplate):
+
+    def upload_path(self, filename):
+        return os.path.join('private/', self.profile.user.username, 'posts', str(self.id), filename)
+
+    title = models.CharField(max_length=200)
+    media = models.FileField(upload_to=upload_path)
+
+
+class Comment(UserReactionTemplate):
 
     def upload_path(self, filename):
         return os.path.join('private/', self.profile.user.username, 'comments', f'post{self.post.id}', str(self.id),
                             filename)
 
-    profile = models.ForeignKey('account.Profile', related_name='comments', on_delete=models.CASCADE)
-    body = models.TextField()
     media = models.FileField(upload_to=upload_path)
-    create_date = models.DateTimeField(auto_now_add=True)
-    update_date = models.DateTimeField(auto_now=True)
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
     replies = models.ForeignKey('self', related_name='comments', on_delete=models.CASCADE)
+
+
+class Like(models.Model):
+    from ..account.models import Profile
+    target = models.ForeignKey(UserReactionTemplate, related_name='likes', on_delete=models.CASCADE)
+    liker = models.ForeignKey(Profile, related_name='likes', on_delete=models.CASCADE)
