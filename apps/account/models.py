28123818050cc4b4cd1c5c21d35467040653e507
alireza <alirezashateri7@gmail.com>
2020-01-08 17:08:59 +0330

    Add Follow models to account app

--- apps/account/models.py
+++ apps/account/models.py
@@ -8,3 +8,18 @@ class Profile(models.Model):
     age = models.IntegerField()
     telephone_number = PhoneNumberField(unique=True)
 
+
+class Follow(models.Model):
+    source = models.ForeignKey(Profile, related_name='follows', on_delete=models.CASCADE)
+
+    class Meta:
+        abstract = True
+
+
+class FollowUser(Follow):
+    target = models.ForeignKey(Profile, related_name='followers', on_delete=models.CASCADE)
+
+
+class FollowChannel(Follow):
+    from ..channel.models import Channel
+    target = models.ForeignKey(Channel, related_name='followers', on_delete=models.CASCADE)
--- apps/channel/models.py
+++ apps/channel/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Channel(models.Model):
+    from ..account.models import Profile
+    creator = models.ForeignKey(Profile, related_name='channels', on_delete=models.CASCADE)
