3e6ed89b58c5f66070116ba8fdf9415d88ed2671
Rez <reza.amini.majd1379@gmail.com>
2020-01-10 02:59:00 +0330

    Account View started

--- apps/account/views.py
+++ apps/account/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework.generics import GenericAPIView
 
-# Create your views here.
+
+class SignUpView(GenericAPIView):
+    pass
+
+
+class SingInView(GenericAPIView):
+    pass
+
+
+class LogoutView(GenericAPIView):
+    pass
+
+
+class ResetPasswordView(GenericAPIView):
+    pass
+
+
+class ProfileView(GenericAPIView):
+    pass
+
+
+class FollowView(GenericAPIView):
+    pass
