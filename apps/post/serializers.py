ee9379c476150884ccf22cff17a7c9f31fc6fae6
alireza <alirezashateri7@gmail.com>
2020-01-10 03:01:55 +0330

    Add Post Serializer

--- apps/post/serializers.py
+++ apps/post/serializers.py
@@ -5,11 +5,32 @@ from apps.post import models as post_models
 
 
 class PostAsListItemSerializer(ModelSerializer):
-    pass
+    summary = SerializerMethodField('_summary')
+    post_owner = SerializerMethodField('_post_owner')
+
+    @staticmethod
+    def _summary(post: post_models.Post):
+        return post.post_summary
+
+    @staticmethod
+    def _post_owner(post: post_models.Post):
+        return post.profile.user.username
+
+    class Meta:
+        model = post_models.Post
+        fields = ['type', 'title', 'post_owner', 'summary', 'media', 'create_date', 'update_date']
 
 
 class PostSerializer(ModelSerializer):
-    pass
+    post_owner = SerializerMethodField('_post_owner')
+
+    @staticmethod
+    def _post_owner(post: post_models.Post):
+        return post.profile.user.username
+
+    class Meta:
+        model = post_models.Post
+        fields = ['type', 'title', 'post_owner', 'body', 'media', 'create_date', 'update_date']
 
 
 class CommentSerializer(ModelSerializer):
