051f5041a06bd14a0ffc8115d386e0660844d1d0
Rez <reza.amini.majd1379@gmail.com>
2020-01-10 02:50:23 +0330

    follow seriallizers compelte

--- apps/account/serializers.py
+++ apps/account/serializers.py
@@ -1,11 +1,16 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
+from rest_framework.serializers import SerializerMethodField
 from rest_framework.validators import UniqueValidator
 from .models import *
+from rest_polymorphic.serializers import PolymorphicSerializer
 
 
 class UserSerializers(serializers.ModelSerializer):
-    pass
+    email = serializers.EmailField(validators=[UniqueValidator(queryset=User.objects.all())])
+
+    class Meta:
+        model: User
+        fields = ['email', 'username', 'password']
 
 
 class ProfileSerializers(serializers.ModelSerializer):
@@ -17,23 +22,44 @@ class ProfileSerializers(serializers.ModelSerializer):
 
 
 class FollowSerializers(serializers.ModelSerializer):
+    source_name = SerializerMethodField('_source_name')
+
+    @staticmethod
+    def _source_name(follow: Follow):
+        return follow.source.user.username
+
     class Meta:
         model = Follow
-        fields = []
+        fields = ['source_name']
 
 
 class FollowUserSerializers(serializers.ModelSerializer):
+    target_name = SerializerMethodField('_target_name')
+
+    @staticmethod
+    def _target_name(follow: FollowUser):
+        return follow.target.user.username
 
     class Meta:
         model = FollowUser
-        fields = ['target']
+        fields = ['target_name']
 
 
 class FollowChannelSerializers(serializers.ModelSerializer):
+    target_name = SerializerMethodField('_target_name')
+
+    @staticmethod
+    def _target_name(follow: FollowUser):
+        return follow.target.user.username
+
     class Meta:
         model = FollowChannel
-        fields = ['target']
+        fields = ['target_name']
 
 
-class PolymorphicFollowSerializers(serializers.ModelSerializer):
-    pass
+class PolymorphicFollowSerializers(PolymorphicSerializer):
+    follow_serializers = {
+        Follow: FollowSerializers,
+        FollowUser: FollowUserSerializers,
+        FollowChannel: FollowChannelSerializers
+    }
