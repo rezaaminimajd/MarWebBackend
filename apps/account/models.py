bdfe027adcd36953385ae61629c60ee192786c48
alireza <alirezashateri7@gmail.com>
2020-01-09 11:04:58 +0330

    Comment and Post Fix

--- apps/account/models.py
+++ apps/account/models.py
@@ -2,6 +2,17 @@ from django.contrib.auth.models import User
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 
+from polymorphic.models import PolymorphicModel
+
+
+class FollowTypes:
+    CHANNEL = 'channel'
+    USER = 'user'
+    TYPES = (
+        (CHANNEL, 'Channel Follow'),
+        (USER, 'User Follow')
+    )
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, related_name='profile', on_delete=models.CASCADE)
@@ -9,17 +20,29 @@ class Profile(models.Model):
     telephone_number = PhoneNumberField(unique=True)
 
 
-class Follow(models.Model):
-    source = models.ForeignKey(Profile, related_name='follows', on_delete=models.CASCADE)
-
-    class Meta:
-        abstract = True
+class Follow(PolymorphicModel):
+    source = models.ForeignKey(Profile, related_name='followings', on_delete=models.CASCADE)
+    type = models.CharField(max_length=20, choices=FollowTypes.TYPES)
 
 
 class FollowUser(Follow):
     target = models.ForeignKey(Profile, related_name='followers', on_delete=models.CASCADE)
 
+    def pre_save(self):
+        self.type = FollowTypes.USER
+
+    def save(self, *args, **kwargs):
+        self.pre_save()
+        super().save(*args, **kwargs)
+
 
 class FollowChannel(Follow):
     from ..channel.models import Channel
     target = models.ForeignKey(Channel, related_name='followers', on_delete=models.CASCADE)
+
+    def pre_save(self):
+        self.type = FollowTypes.CHANNEL
+
+    def save(self, *args, **kwargs):
+        self.pre_save()
+        super().save(*args, **kwargs)
--- apps/post/models.py
+++ apps/post/models.py
@@ -2,19 +2,21 @@ import os
 
 from django.db import models
 
+from polymorphic.models import PolymorphicModel
+
 
 # Create your models here.
 
 
-class UserReactionTemplate(models.Model):
+class UserActionTemplate(PolymorphicModel):
     from ..account.models import Profile
-    profile = models.ForeignKey(Profile, related_name='posts', on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, related_name='actions', on_delete=models.CASCADE)
     body = models.TextField()
     create_date = models.DateTimeField(auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True)
 
 
-class Post(UserReactionTemplate):
+class Post(UserActionTemplate):
 
     def upload_path(self, filename):
         return os.path.join('private/', self.profile.user.username, 'posts', str(self.id), filename)
@@ -23,18 +25,18 @@ class Post(UserReactionTemplate):
     media = models.FileField(upload_to=upload_path)
 
 
-class Comment(UserReactionTemplate):
+class Comment(UserActionTemplate):
 
     def upload_path(self, filename):
         return os.path.join('private/', self.profile.user.username, 'comments', f'post{self.post.id}', str(self.id),
                             filename)
 
     media = models.FileField(upload_to=upload_path)
-    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
-    replies = models.ForeignKey('self', related_name='comments', on_delete=models.CASCADE)
+    post_related = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    replies = models.ForeignKey('self', related_name='comments', on_delete=models.CASCADE, null=True, blank=True)
 
 
 class Like(models.Model):
     from ..account.models import Profile
-    target = models.ForeignKey(UserReactionTemplate, related_name='likes', on_delete=models.CASCADE)
+    target = models.ForeignKey(UserActionTemplate, related_name='likes', on_delete=models.CASCADE)
     liker = models.ForeignKey(Profile, related_name='likes', on_delete=models.CASCADE)
