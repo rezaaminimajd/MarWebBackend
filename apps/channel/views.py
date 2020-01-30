b4b688a59e18f04f039deb0d94d4ab25d941c74a
Mehrdad Saberi <merhdads@gmail.com>
2020-01-30 12:14:38 +0330

    view for channel

--- apps/channel/views.py
+++ apps/channel/views.py
@@ -3,13 +3,32 @@ from django.shortcuts import render
 from rest_framework.generics import GenericAPIView
 
 
+from . import models as channel_models
+from . import serializers as channel_serializers
+
 # Create your views here.
 
 
 class ChannelsListAPIView(GenericAPIView):
+    queryset = channel_models.Channel.objects.all()
+    serializer_class = channel_serializers.ChannelAsListItemSerializer
 
     def get(self, request):
-        pass
+        channels = channel_models.Channel.objects.all()
+        data = self.get_serializer(channels, many=True, read_only=True).data
+        print('data:', data)
+        return Response(data={'channels': data}, status=status.HTTP_200_OK)
+
+class ChannelsSearchListAPIView(GenericAPIView):
+    queryset = channel_models.Channel.objects.all()
+    serializer_class = channel_serializers.ChannelAsListItemSerializer
+
+    def get(self,request,searchQuery):
+        channels = channel_models.Channel.objects.filter(name__startswith=searchQuery)
+        data = self.get_serializer(channels, many=True).data
+        print('data:', data)
+        return Response(data={'channels': data}, status=status.HTTP_200_OK)
+        
 
 
 class ChannelAPIView(GenericAPIView):
