7a3745030757ee928d24d81022d599f22f469152
alireza <alirezashateri7@gmail.com>
2020-02-03 15:39:55 +0330

    Fix post models

--- apps/account/models.py
+++ apps/account/models.py
@@ -23,12 +23,12 @@ class Profile(models.Model):
 
 
 class Follow(PolymorphicModel):
-    source = models.ForeignKey('account.Profile', related_name='followings', on_delete=models.CASCADE)
+    source = models.ForeignKey(User, related_name='followings', on_delete=models.CASCADE)
     follow_type = models.CharField(max_length=20, choices=FollowTypes.TYPES)
 
 
 class FollowUser(Follow):
-    target = models.ForeignKey('account.Profile', related_name='followers_user', on_delete=models.CASCADE)
+    target = models.ForeignKey(User, related_name='followers_user', on_delete=models.CASCADE)
 
     def pre_save(self):
         self.follow_type = FollowTypes.USER
