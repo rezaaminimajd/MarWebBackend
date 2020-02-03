7cb4b9d67475df32ac634135f37db227347431e5
alireza <alirezashateri7@gmail.com>
2020-02-03 22:35:13 +0330

    Fix url bugs

--- apps/channel/urls.py
+++ apps/channel/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'channel'
 
 urlpatterns = [
-    path('channels', views.UserChannelsListAPIView.as_view(), 'user_channels_list'),
-    path('searchchannels/<slug:search>', views.ChannelsSearchListAPIView.as_view(), 'search_channels'),
+    path('channels', views.UserChannelsListAPIView.as_view(), name='user_channels_list'),
+    path('searchchannels/<slug:search>', views.ChannelsSearchListAPIView.as_view(), name='search_channels'),
     path('followchannel', views.FollowAPIView.as_view(), name='follow_channel'),
     path('channel/create', views.ChannelAPIView.as_view(), name='create_channel'),
 ]
