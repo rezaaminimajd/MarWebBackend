159b2761754c1831c2d8cfe3a2d3befc8a0dd610
Mehrdad Saberi <merhdads@gmail.com>
2020-02-03 15:02:15 +0330

    debug post

--- apps/post/models.py
+++ apps/post/models.py
@@ -49,7 +49,7 @@ class Post(UserActionTemplate):
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return f'id:{self.id}, username:{self.profile.user.username}'
+        return f'id:{self.id}, username:{self.user.username}'
 
 
 class Comment(UserActionTemplate):
