9148314a75abd533897ed35a5805debd7b721c4c
alireza <alirezashateri7@gmail.com>
2020-02-04 21:20:59 +0330

    User Channel list by user name apiview

--- apps/channel/urls.py
+++ apps/channel/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'channel'
 
 urlpatterns = [
-    path('channels', views.UserChannelsListAPIView.as_view(), name='user_channels_list'),
+    path('channels/<username>', views.UserChannelsListAPIView.as_view(), name='user_channels_list'),
     path('searchchannels/<slug:search>', views.ChannelsSearchListAPIView.as_view(), name='search_channels'),
     path('followchannel', views.FollowAPIView.as_view(), name='follow_channel'),
     path('channel/create', views.ChannelAPIView.as_view(), name='create_channel'),
