4d17a738eb8132871b8acdde0bbb5d9b00db9e0d
alireza <alirezashateri7@gmail.com>
2020-02-04 22:10:32 +0330

    seen notifications

--- apps/notification/urls.py
+++ apps/notification/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'notification'
 
 urlpatterns = [
-    path('notifications', views.UserNotificationsAPIView.as_view(), name='user_notifications')
+    path('notifications', views.UserNotificationsAPIView.as_view(), name='user_notifications'),
+    path('seen-notifications', views.UserNotificationsAPIView.as_view(), name='seen_notifications')
 ]
