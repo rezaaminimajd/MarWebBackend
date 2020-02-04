13311463bec04ed55067b2233c6943a9a07f4e01
alireza <alirezashateri7@gmail.com>
2020-02-04 20:15:14 +0330

    remove viewset

--- apps/post/urls.py
+++ apps/post/urls.py
@@ -1,10 +1,5 @@
 from django.urls import path
-from rest_framework import routers
 from . import views
-
-router = routers.DefaultRouter()
-router.register('ez', views.NewPostView, basename='ez')
-
 
 app_name = 'post'
 
@@ -13,6 +8,6 @@ urlpatterns = [
     path('posts/new-posts', views.NewPostsAPIVIew.as_view(), name='new_posts'),
     path('posts/followed-channels-post', views.FollowedChannelsPostsAPIView.as_view(), name='followed_channels_post'),
     path('posts/participated-posts', views.ParticipatedPostsAPIView.as_view(), name='participated_posts'),
-    path('post-view',views.PostAPIView.as_view(),name='post'),
-    path('userposts/<slug:username>',views.UserPostsListAPIView.as_view(),name='user_posts'),
-] + router.urls
+    path('post-view', views.PostAPIView.as_view(), name='post'),
+    path('userposts/<slug:username>', views.UserPostsListAPIView.as_view(), name='user_posts'),
+]
