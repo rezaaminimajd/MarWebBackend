a391fbd201354b155f3a3413aebb3f4852755bcc
alireza <alirezashateri7@gmail.com>
2020-02-07 21:53:33 +0330

    top channels url

--- apps/channel/urls.py
+++ apps/channel/urls.py
@@ -6,6 +6,7 @@ app_name = 'channel'
 urlpatterns = [
     path('channels/<username>', views.UserChannelsListAPIView.as_view(), name='user_channels_list'),
     path('channel/create', views.ChannelAPIView.as_view(), name='create_channel'),
+    path('top-channels', views.TopChannelsAPIView.as_view(), name='top_channel'),
     path('channel/<int:channel_id>', views.ChannelAPIView.as_view(), name='channel'),
     path('followchannel/<int:channel_id>', views.FollowAPIView.as_view(), name='follow_channel'),
 ]
